60fe015a3afcf195a2929b1aab6e9b896e49702f
nois_vinhnguyen <vinh.nguyen@newoceaninfosys.com>
2017-06-01 16:34:31 +0700

    Form Validate

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {PeopleListComponent} from './people-list.component';
-import {PersonDetailsComponent} from './person-details.component';
+import { FormsModule} from '@angular/forms';
+
 import { AppComponent }  from './app.component';
 import {PeopleService} from './people.service';
+import {PeopleListComponent} from './people-list.component';
+import {PersonDetailsComponent} from './person-details.component';
 
 import {routing} from './app.routes';
 
 @NgModule({
-  imports: [BrowserModule, routing],
+  imports: [BrowserModule, routing, FormsModule],
   declarations: [AppComponent, PeopleListComponent, PersonDetailsComponent],
   bootstrap: [AppComponent],
   providers: [PeopleService]
