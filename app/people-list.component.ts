3094d6d0906901c956664855d6ec1552616b64f6
nois_vinhnguyen <vinh.nguyen@newoceaninfosys.com>
2017-06-02 18:17:19 +0700

    call api

--- app/people-list.component.ts
+++ app/people-list.component.ts
@@ -1,27 +1,39 @@
 /**
  * Created by davidvinhit on 5/30/2017.
  */
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Person} from './person';
 import {PeopleService} from './people.service';
 
 @Component({
   selector: 'people-list',
   template: `
-  <ul>
-    <li *ngFor="let person of people">
-    <a href="#" [routerLink]="['/persons',person.id]">
-    {{person.name}}
-    </a>
-    </li>
-  </ul>
+    <ul>
+      <li *ngFor="let person of people">
+        <a href="#" [routerLink]="['/persons',person.id]">
+          {{person.name}}
+        </a>
+      </li>
+    </ul>
+    <section *ngIf="errorMessage">
+      {{errorMessage}}
+    </section>
   `
 })
 
-export class PeopleListComponent {
-  people:Person[] = [];
+export class PeopleListComponent implements OnInit {
+  people: Person[] = [];
+  errorMessage: string = '';
 
-  constructor(private _peopleService:PeopleService) {
-    this.people = this._peopleService.getAll();
+  constructor(private _peopleService: PeopleService) {
+
+  }
+
+  ngOnInit() {
+    this._peopleService.getAll().subscribe(
+      p => this.people = p,
+      e=>this.errorMessage = e
+    );
+
   }
 }
